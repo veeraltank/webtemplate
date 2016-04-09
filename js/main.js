b643ae6869f0220b492db63dc1f9e96b61cb59ae
Macster <macster@Love-Haight-Macs-6.local>
2016-04-09 14:46:18 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//When the document is ready, do this thing
+$(document).ready(function() {
+	//When the button is clicked, do this thing
+	$("#btn").click(function() {
+		//Hide all h1 elements
+		$('div').addClass('divEl')
+	})
+})
